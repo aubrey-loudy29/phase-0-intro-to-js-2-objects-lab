28022a135ab53fb9f34e6b64db72222ca2bd92d0
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-02-13 13:44:17 -0700

    commit tree b725daf5f76bf4fde6663424afa50226a2972dcc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employees = {
+    employee: {
+        name: 'Caleb',
+        streetAddress: '3014'
+    },
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return employees.employee.name = 'Sam'
+}
